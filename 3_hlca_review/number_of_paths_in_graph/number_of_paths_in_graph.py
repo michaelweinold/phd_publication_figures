01b4c4e849aabf8fc1d04f2d7ed0701c1c7c9eb6
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2023-05-24 16:23:46 +0200

    almost completed figure

--- 3_hlca_review/number_of_paths_in_graph/number_of_paths_in_graph.py
+++ 3_hlca_review/number_of_paths_in_graph/number_of_paths_in_graph.py
@@ -17,6 +17,8 @@ import numpy as np
 import pandas as pd
 import math
 
+# type hints
+from typing import Tuple
 
 # i/o
 from pathlib import PurePath, Path
@@ -32,17 +34,21 @@ plt.rcParams.update({
 
 # DATA GENERATION ################################
 
-def number_of_paths_power_series(n, l) -> int:
+def number_of_paths_two_nodes_graph_theory(n, l) -> int:
     '''
     Returns the number of paths of length l
     associated with an input-output-table with n sectors.
     Compare: https://math.stackexchange.com/a/4704603/
     '''
+    result: int = 0
     for k in range(n-l, n-1): # mathematically: k=[n-l-1, n-2]
-        result += math.factorial(n-2) * 1/math.factorial(k)
+        if n-l >0:
+            result += math.factorial(n-2) * 1/math.factorial(k)
+        else:
+            result += 0
     return result
 
-def number_of_paths_graph_theory(n, omega) -> int:
+def number_of_paths_two_nodes_power_series(n, omega) -> int:
     '''
     Returns the number of omega-order paths
     associated with an input-output-table with n sectors.
@@ -50,11 +56,35 @@ def number_of_paths_graph_theory(n, omega) -> int:
     '''
     return n**(omega-1)
 
+def create_dataframe_number_of_paths(
+        n_max: int = 300,
+        param_max: int = 10,
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    '''
+    Creates a dataframe with the number of paths
+    for all combinations of n and omega or l.
+    '''
+
+    df_power_series = pd.DataFrame()
+    df_graph_theory = pd.DataFrame()
+    index_series = pd.Series(range(1, n_max+1))
+    df_power_series.index = index_series
+    df_graph_theory.index = index_series
+    
+    for param in range(1, param_max+1):
+        column_power_series = pd.Series()
+        column_graph_theory = pd.Series()
+        for n in range(1, n_max+1):
+            column_power_series.loc[n]=number_of_paths_two_nodes_power_series(n, param)
+            column_graph_theory.loc[n]=number_of_paths_two_nodes_graph_theory(n, param)
+        df_power_series[param]=column_power_series
+        df_graph_theory[param]=column_graph_theory
+
+    return df_power_series, df_graph_theory
+
+df_power_series, df_graph_theory = create_dataframe_number_of_paths()
+
 # DATA MANIPULATION #############################
-'''
-Normalize data to 1950 = 1
-'''
-df_jetfuel['WPU0572_normalized_1950'] = df_jetfuel['WPU0572'] / df_jetfuel['WPU0572'].loc[df_jetfuel['DATE'] == '1950-01-01'].values[0]
 
 # FIGURE ########################################
 
@@ -64,50 +94,107 @@ fig, ax = plt.subplots(
         num = 'main',
         nrows = 1,
         ncols = 1,
+        sharey=True,
         dpi = 300,
         figsize=(30*cm, 10*cm), # A4=(210x297)mm
     )
 
 # DATA #######################
 
-x_jetfuel = df_jetfuel['DATE']
-y_jetfuel = df_jetfuel['WPU0572_normalized_1950']
+# AXIS SCALE #################
+
+ax.set_yscale('log')
 
 # AXIS LIMITS ################
 
-plt.xlim(
-    datetime.strptime('1950', '%Y'),
-    datetime.strptime('2023', '%Y')
-)
-ax.set_ylim(1,80)
+ax.set_xlim(0,300)
 
 # TICKS AND LABELS ###########
 
-ax.minorticks_on()
-ax.tick_params(axis='x', which='minor', bottom=False)
+ax.tick_params(axis='y', which='both', bottom=False)
+ax.tick_params(axis='x', which='both', bottom=False)
 
 # GRIDS ######################
 
-ax.grid(which='both', axis='y', linestyle='-', linewidth = 0.5)
-ax.grid(which='major', axis='x', linestyle='--', linewidth = 0.5)
+ax.grid(True, which='both', axis='y', linestyle='-', linewidth = 0.5)
+ax.grid(True, which='both', axis='x', linestyle='-', linewidth = 0.5)
 
 # AXIS LABELS ################
 
-ax.set_xlabel("Year")
-ax.set_ylabel("Producer Price Index \n (Aviation Fuel)")
+ax.set_ylabel("Number of Paths $(p)$")
+ax.set_xlabel("Number of Sectors $(n)$")
 
 # PLOTTING ###################
 
-
 ax.plot(
-    x_jetfuel,
-    y_jetfuel,
+    df_power_series.index,
+    df_power_series[2],
     color = 'black',
     linewidth = 1,
-    label = 'Jet Fuel (Kerosene)'
+    linestyle = 'solid',
+    label = '$\omega=2$'
+)
+ax.plot(
+    df_power_series.index,
+    df_power_series[4],
+    color = 'black',
+    linewidth = 1,
+    linestyle = 'dashdot',
+    label = '$\omega=4$'
+)
+ax.plot(
+    df_power_series.index,
+    df_power_series[6],
+    color = 'black',
+    linewidth = 1,
+    linestyle = 'dashed',
+    label = '$\omega=6$'
+)
+ax.plot(
+    df_power_series.index,
+    df_power_series[8],
+    color = 'black',
+    linewidth = 1,
+    linestyle = 'dotted',
+    label = '$\omega=8$'
+)
+
+ax.plot(
+    df_graph_theory.index,
+    df_graph_theory[2],
+    color = 'blue',
+    linewidth = 1,
+    linestyle = 'solid',
+    label = '$l=2$'
+)
+ax.plot(
+    df_graph_theory.index,
+    df_graph_theory[4],
+    color = 'blue',
+    linewidth = 1,
+    linestyle = 'dashdot',
+    label = '$l=4$'
+)
+ax.plot(
+    df_graph_theory.index,
+    df_graph_theory[6],
+    color = 'blue',
+    linewidth = 1,
+    linestyle = 'dashed',
+    label = '$l=6$'
+)
+ax.plot(
+    df_graph_theory.index,
+    df_graph_theory[8],
+    color = 'blue',
+    linewidth = 1,
+    linestyle = 'dotted',
+    label = '$l=8$'
 )
 
 # LEGEND ####################
+
+ax.legend()
 
 # EXPORT #########################################
 
