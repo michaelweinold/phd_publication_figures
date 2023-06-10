5471ef243878b4f97e2dbfaa6568ec82102a43f1
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2023-06-10 13:45:44 +0200

    finished figure

--- 4_aviation/profitability_and_fuel_price/profitability.py
+++ 4_aviation/profitability_and_fuel_price/profitability.py
@@ -30,9 +30,9 @@ plt.rcParams.update({
 
 # DATA IMPORT ###################################
 
-df = pd.read_excel(
+df_profit = pd.read_excel(
     io = 'data/data.xlsx',
-    sheet_name = 'Sheet1',
+    sheet_name = 'Profit',
     usecols = lambda column: column in [
         'year',
         'plot net profit (2022 BUSD)'
@@ -44,6 +44,20 @@ df = pd.read_excel(
     header = 0,
     engine = 'openpyxl'
 )
+df_fuelprice = pd.read_excel(
+    io = 'data/data.xlsx',
+    sheet_name = 'Fuel Price',
+    usecols = lambda column: column in [
+        'year',
+        'kerosene producer price index (100=2022)'
+    ],
+    dtype={
+        'year': int,
+        'kerosene producer price index (100=2022)': float,
+    },
+    header = 0,
+    engine = 'openpyxl'
+)
 
 # DATA MANIPULATION #############################
 
@@ -51,13 +65,14 @@ df = pd.read_excel(
 
 # SETUP ######################
 
-fig, ax = plt.subplots(
+fig, ax1 = plt.subplots(
         num = 'main',
         nrows = 1,
         ncols = 1,
         dpi = 300,
         figsize=(30*cm, 10*cm), # A4=(210x297)mm
 )
+ax2 = ax1.twinx()
 
 # DATA #######################
 
@@ -65,8 +80,9 @@ fig, ax = plt.subplots(
 
 # AXIS LIMITS ################
 
-ax.set_xlim(1949,2024)
-ax.set_ylim(-50,50)
+ax1.set_xlim(1949,2024)
+ax1.set_ylim(-50,50)
+ax2.set_ylim(0,110)
 
 # COLORBAR ###################
 
@@ -74,32 +90,47 @@ ax.set_ylim(-50,50)
 
 # TICKS AND LABELS ###########
 
-ax.minorticks_on()
-ax.tick_params(axis='x', which='both', bottom=False)
-ax.tick_params(axis='y', which='both', bottom=False)
+ax1.minorticks_on()
+ax1.tick_params(axis='x', which='both', bottom=False)
+ax1.tick_params(axis='y', which='both', bottom=False)
 
 # GRIDS ######################
 
-ax.grid(which='both', axis='y', linestyle='-', linewidth = 0.5)
-ax.grid(which='both', axis='x', linestyle='-', linewidth = 0.5)
+ax1.grid(which='both', axis='y', linestyle='-', linewidth = 0.5)
+ax1.grid(which='both', axis='x', linestyle='-', linewidth = 0.5)
 
 # AXIS LABELS ################
 
-ax.set_xlabel("Year")
-ax.set_ylabel("Airline Net Profit [2022 BUSD]")
+ax1.set_xlabel("Year")
+ax1.set_ylabel("Airline Net Profit [2022 BUSD]")
+ax2.set_ylabel("U.S. Producer Price Index (2022=100)")
 
 # PLOTTING ###################
 
-ax.bar(
-    x = df['year'],
-    height = df['plot net profit (2022 BUSD)'],
+ax1.bar(
+    x = df_profit['year'],
+    height = df_profit['plot net profit (2022 BUSD)'],
     width = 0.8,
     bottom = None,
     align = 'center',
+    label = 'World Airlines Net Profit'
+)
+ax2.plot(
+    df_fuelprice['year'],
+    df_fuelprice['kerosene producer price index (100=2022)'],
+    color = 'black',
+    label = 'Kerosene and Jet Fuels'
 )
 
 # LEGEND ####################
 
+lines, labels = ax1.get_legend_handles_labels()
+lines2, labels2 = ax2.get_legend_handles_labels()
+ax2.legend(
+    lines + lines2,
+    labels + labels2,
+    loc='upper left',
+)
 
 # TITLE #####################
 
