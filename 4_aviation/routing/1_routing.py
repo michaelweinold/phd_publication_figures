8a34b8c6856f02ddfa4f376b91d64df260afd5e3
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2023-08-05 08:30:09 +0200

    updated figure

--- 4_aviation/routing/1_routing.py
+++ 4_aviation/routing/1_routing.py
@@ -7,12 +7,14 @@
 # plotting
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as font_manager
-from mpl_toolkits.basemap import Basemap
+# geographic plotting
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import shapely.geometry as sgeom
 # unit conversion
 cm = 1/2.54 # for inches-cm conversion
 # time manipulation
 from datetime import datetime
-
 # data science
 import numpy as np
 import pandas as pd
@@ -36,41 +38,57 @@ df_finnair = pd.read_csv(
     header = 'infer',
     index_col = False,
 )
+df_british = pd.read_csv(
+    filepath_or_buffer = 'data/BA57_301ab0b0.csv',
+    sep = ',',
+    header = 'infer',
+    index_col = False,
+)
 
 # DATA MANIPULATION #############################
 
-finnair_coords = df_finnair['Position'].str.split(',', expand=True)
-finnair_lats = finnair_coords[0].astype(float).values
-finnair_lons = finnair_coords[1].astype(float).values
+def extract_coordinates_from_csv(
+        df: pd.DataFrame,
+        column_name: str = 'Position'
+) -> pd.DataFrame:
+    df_coords: pd.DataFrame = df[column_name].str.split(',', expand=True)
+    df_coords.columns = ['lat', 'lon']
+    ls_coords = sgeom.LineString(
+        zip(
+            df_coords['lon'].astype(float),
+            df_coords['lat'].astype(float)
+        )
+    )
+    return ls_coords
+
+finnair_coords = extract_coordinates_from_csv(
+    df = df_finnair,
+    column_name = 'Position'
+)
+british_coords = extract_coordinates_from_csv(
+    df = df_british,
+    column_name = 'Position'
+)
 
 # FIGURE ########################################
 
 # SETUP ######################
 
-fig, ax = plt.subplots(
-        num = 'main',
-        nrows = 1,
-        ncols = 1,
-        dpi = 300,
-        figsize=(30*cm, 15*cm), # A4=(210x297)mm
-    )
-
-m = Basemap(
-    projection='mill',
-    llcrnrlat=-70,
-    urcrnrlat=75,
-    llcrnrlon=-180,
-    urcrnrlon=180,
-    resolution='c'
+fig = plt.figure(
+    num = 'main',
+    dpi = 300,
+    figsize=(30*cm, 10*cm), # A4=(210x297)mm
+)
+ax = plt.subplot(
+    projection=ccrs.PlateCarree(),
 )
 
-m.drawcoastlines()
-m.drawcountries()
-m.fillcontinents(color='0.8', lake_color=None, ax=None, zorder=None, alpha=None)
+# https://scitools.org.uk/cartopy/docs/latest/gallery/lines_and_polygons/features.html#features
+ax.add_feature(cfeature.BORDERS, alpha=0.25)
+ax.add_feature(cfeature.LAKES, alpha=0.5)
+ax.add_feature(cfeature.COASTLINE)
 
 # DATA #######################
-
-finnair_x, finnair_y = m(finnair_lons, finnair_lats)
 
 # AXIS LIMITS ################
 
@@ -82,8 +100,20 @@ finnair_x, finnair_y = m(finnair_lons, finnair_lats)
 
 # PLOTTING ###################
 
-m.plot(finnair_x, finnair_y, 'r-', linewidth=2, markersize=5, label='Actual Track')
-m.drawgreatcircle(finnair_lons[0], finnair_lats[0], finnair_lons[-1], finnair_lats[-1], del_s=300, color='blue', linewidth = 2, label='Great Circle Distance')
+ax.add_geometries(
+    geoms = finnair_coords,
+    crs = ccrs.PlateCarree(),
+    facecolor = 'none',
+    edgecolor = 'red',
+    linewidth = 1
+)
+ax.add_geometries(
+    geoms = british_coords,
+    crs = ccrs.PlateCarree(),
+    facecolor = 'none',
+    edgecolor = 'red',
+    linewidth = 1
+)
 
 # LEGEND ####################
 
