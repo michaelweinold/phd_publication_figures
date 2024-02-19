98288df4c0a435c05d38002facbf819eff48c8e2
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2024-02-19 13:33:51 +0100

    added data

---
+++ 4_aviation/metric_value/metric_value.py
@@ -0,0 +1,98 @@
+#%%
+# runs code as interactive cell 
+# https://code.visualstudio.com/docs/python/jupyter-support-py
+
+# IMPORTS #######################################
+
+# sys
+import os
+# plotting
+import matplotlib.pyplot as plt
+# unit conversion
+cm = 1/2.54 # for inches-cm conversion
+# time manipulation
+from datetime import datetime
+# data science
+import numpy as np
+import pandas as pd
+
+# SETUP #########################################
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Arial",
+    "font.sans-serif": "Computer Modern",
+    'font.size': 11
+})
+
+# DATA IMPORT ###################################
+
+
+# DATA MANIPULATION #############################
+
+# FIGURE ########################################
+
+# SETUP ######################
+
+fig, ax = plt.subplots(
+    num = 'main',
+    nrows = 1,
+    ncols = 1,
+    dpi = 300,
+    figsize=(30*cm, 5*cm), # A4=(210x297)mm,
+)
+
+# SECONDARY AXES ##############
+
+# AXIS LIMITS ################
+
+ax.set_xlim(
+    datetime(2005, 1, 1),  # Start date
+    datetime(2024, 12, 31)  # End date
+)
+
+# TICKS AND LABELS ###########
+
+ax.minorticks_on()
+ax.tick_params(axis='x', which='minor', bottom=True)
+
+# GRIDS ######################
+
+ax.grid(which='major', axis='y', linestyle='-', linewidth = 0.5)
+ax.grid(which='minor', axis='y', linestyle=':', linewidth = 0.5)
+ax.grid(which='major', axis='x', linestyle='-', linewidth = 0.5)
+ax.grid(which='minor', axis='x', linestyle=':', linewidth = 0.5)
+
+# AXIS LABELS ################
+
+ax.set_ylabel("Change Relative to 1950 [1]")
+
+# PLOTTING ###################
+
+ax.plot(
+    df_google['YEARMONTH'],
+    df_google['SEARCHINTEREST'],
+    color = 'black',
+    linestyle = '-',
+    linewidth = 1.5,
+    label = 'Google Search Interest'
+)
+
+# LEGEND ####################
+
+ax.legend(
+    loc = 'upper left',
+)
+
+# EXPORT #########################################
+
+file_path = os.path.abspath(__file__)
+file_name = os.path.splitext(os.path.basename(file_path))[0]
+figure_name: str = str(file_name + '.pdf')
+
+plt.savefig(
+    fname = figure_name,
+    format="pdf",
+    bbox_inches='tight',
+    transparent = False
+)
