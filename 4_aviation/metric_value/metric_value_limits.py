50a54936364a794c78008c4d9d08ebf1f4fe92a0
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2024-02-19 18:31:09 +0100

    completed figure

--- 4_aviation/metric_value/metric_value_limits.py
+++ 4_aviation/metric_value/metric_value_limits.py
@@ -15,6 +15,7 @@ from datetime import datetime
 # data science
 import numpy as np
 import pandas as pd
+import pandas as pd
 
 # SETUP #########################################
 
@@ -27,61 +28,18 @@ plt.rcParams.update({
 
 # DATA IMPORT ###################################
 
-df_trendline = pd.read_excel(
-    io = 'data/data.xlsx', 
-    sheet_name = 'trendline',
-    usecols = lambda column: column in [
-        'year',
-        'metric value [kg/km]',
-    ],
-    dtype={
-        'year': float,
-        'metric value [kg/km]': float,
-    },
-    header = 0,
-    engine = 'openpyxl',
-    decimal='.'
-)
-df_sa_acft = pd.read_excel(
-    io = 'data/data.xlsx', 
-    sheet_name = 'sa_acft',
-    usecols = lambda column: column in [
-        'year',
-        'metric value [kg/km]',
-    ],
-    dtype={
-        'year': int,
-        'metric value [kg/km]': float,
-    },
-    header = 0,
-    engine = 'openpyxl',
-    decimal='.'
-)
-df_sta_acft = pd.read_excel(
-    io = 'data/data.xlsx', 
-    sheet_name = 'sta_acft',
-    usecols = lambda column: column in [
-        'year',
-        'metric value [kg/km]',
-    ],
-    dtype={
-        'year': int,
-        'metric value [kg/km]': float,
-    },
-    header = 0,
-    engine = 'openpyxl',
-    decimal='.'
-)
 df_my_acft = pd.read_excel(
     io = 'data/data.xlsx', 
     sheet_name = 'my_acft',
     usecols = lambda column: column in [
-        'year',
+        'MTOW [kg]',
         'metric value [kg/km]',
+        'shortname',
     ],
     dtype={
-        'year': int,
+        'MTOW [kg]': int,
         'metric value [kg/km]': float,
+        'shortname': str,
     },
     header = 0,
     engine = 'openpyxl',
@@ -89,6 +47,48 @@ df_my_acft = pd.read_excel(
 )
 
 # DATA MANIPULATION #############################
+
+# source: ICAO Annex 16, Vol. 3, Chapter 2.4 ff.
+
+def mv_limit_1(x: float) -> float:
+    return 10**(-2.73780+(0.681310*np.log10(x))+(-0.0277861*np.log10(x)**2))
+
+def mv_limit_2(x: float) -> float:
+    return 10**(-1.412742+(-0.020517*np.log10(x))+(0.0593831*np.log10(x)**2))
+
+
+# Generate x values from 0 to 150
+x_values_1 = np.linspace(0, 60000, 50)
+y_values_1 = [mv_limit_1(y) for y in x_values_1]
+x_values_2 = np.linspace(60000, 70394, 2)
+y_values_2 = np.linspace(0.764, 0.764, 2)
+x_values_3 = np.linspace(70395, 600000, 50)
+y_values_3 = [mv_limit_2(y) for y in x_values_3]
+
+df_1 = pd.DataFrame({
+    'x': np.concatenate((x_values_1, x_values_2, x_values_3)),
+    'y': np.concatenate((y_values_1, y_values_2, y_values_3))
+})
+
+def mv_limit_1_1(x: float) -> float:
+    return 10**(-2.57535+(0.609766*np.log10(x))+(-0.0191302*np.log10(x)**2))
+
+def mv_limit_2_1(x: float) -> float:
+    return 10**(-1.39353+(-0.020517*np.log10(x))+(0.0593831*np.log10(x)**2))
+
+
+# Generate x values from 0 to 150
+x_values_1_1 = np.linspace(0, 60000, 50)
+y_values_1_1 = [mv_limit_1_1(y) for y in x_values_1_1]
+x_values_2_1 = np.linspace(60000, 70106, 2)
+y_values_2_1 = np.linspace(0.797, 0.797, 2)
+x_values_3_1 = np.linspace(70107, 600000, 50)
+y_values_3_1 = [mv_limit_2_1(y) for y in x_values_3_1]
+
+df_1_1 = pd.DataFrame({
+    'x': np.concatenate((x_values_1_1, x_values_2_1, x_values_3_1)),
+    'y': np.concatenate((y_values_1_1, y_values_2_1, y_values_3_1))
+})
 
 # FIGURE ########################################
 
@@ -106,12 +106,14 @@ fig, ax = plt.subplots(
 
 # AXIS LIMITS ################
 
-ax.set_xlim(1960,2040)
+ax.set_xlim(0,600000)
+ax.set_ylim(0,3)
 
 # TICKS AND LABELS ###########
 
 ax.minorticks_on()
 ax.tick_params(axis='x', which='minor', bottom=True)
+
 
 # GRIDS ######################
 
@@ -123,45 +125,41 @@ ax.grid(which='minor', axis='x', linestyle=':', linewidth = 0.5)
 # AXIS LABELS ################
 
 ax.set_ylabel("\"CO$_2$ Emissions Evaluation Metric\" [kg/km]")
+ax.set_xlabel("MTOW [kg]")
 
 # PLOTTING ###################
 
 ax.plot(
-    df_trendline['year'],
-    df_trendline['metric value [kg/km]'],
-    label = 'All Size Average (Delivery Year)',
-    color = 'black',
+    df_1['x'], df_1['y'],
+    color = 'blue',
     linestyle = '-',
     linewidth = 2,
+    label = 'CO$_2$ Limit New Types',
 )
-ax.scatter(
-    df_sa_acft['year'],
-    df_sa_acft['metric value [kg/km]'],
-    label = 'Single-Isle',
-    color = 'blue',
-    marker = 'o',
-)
-ax.scatter(
-    df_sta_acft['year'],
-    df_sta_acft['metric value [kg/km]'],
-    label = 'Small Twin-Aisle',
+ax.plot(
+    df_1_1['x'], df_1_1['y'],
     color = 'red',
-    marker = 'o',
+    linestyle = '-',
+    linewidth = 2,
+    label = 'CO$_2$ Limit Existing Types (in Production)',
 )
-"""
+
+for i, shortname in enumerate(df_my_acft['shortname']):
+    ax.annotate(shortname, (df_my_acft['MTOW [kg]'][i], df_my_acft['metric value [kg/km]'][i]), xytext=(5, 5), textcoords='offset points', bbox=dict(facecolor='white', edgecolor='none'))
+
 ax.scatter(
-    df_my_acft['year'],
+    df_my_acft['MTOW [kg]'],
     df_my_acft['metric value [kg/km]'],
-    label = 'Large Twin-Aisle',
-    color = 'green',
+    label = 'Current Example Aircraft',
+    color = 'black',
     marker = 'o',
+    zorder = 10,
 )
-"""
 
 # LEGEND ####################
 
 ax.legend(
-    loc = 'upper right',
+    loc = 'upper left',
 )
 
 # EXPORT #########################################
@@ -176,3 +174,4 @@ plt.savefig(
     bbox_inches='tight',
     transparent = False
 )
+# %%
